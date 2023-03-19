1142181a33afe6a773484dab6fdda31e0fb8347a
Hasanztuy <128010688+Hasanztuy@users.noreply.github.com>
2023-03-19 14:15:29 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+        public function index()
+        {
+            echo "<h1>Perkenalkan</h1>";
+            echo"Nama saya Hasan Albana
+            Saya tinggal di daerah Pondok Rajeg
+            Olah raga yang saya sukai adalah WorkOut";
+        }
+}
